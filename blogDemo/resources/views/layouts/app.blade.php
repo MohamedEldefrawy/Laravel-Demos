49093eb81dced389479a9027489a8e1278675e5e
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-04-16 13:54:32 +0200

    Create index view

--- blogDemo/resources/views/layouts/app.blade.php
+++ blogDemo/resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
           integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">ITI Blog Post</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
--- blogDemo/resources/views/posts/index.blade.php
+++ blogDemo/resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
     <table class="table mt-4">
         <thead>
         <tr>
-            <th scope="col">#</th>
+            <th scope="col">ID</th>
             <th scope="col">Title</th>
             <th scope="col">Posted By</th>
             <th scope="col">Created At</th>
@@ -19,10 +19,11 @@
         <tbody>
         @foreach ( $posts as $post)
             <tr>
-                <td>{{ $post['id'] }}</th>
-                <td>{{ $post['title'] }}</td>
-                <td>{{ $post['post_creator'] }}</td>
-                <td>{{ $post['created_at'] }}</td>
+                <td>
+                {{ $post['id'] }}</th>
+                <td>{{ $post['Title'] }}</td>
+                <td>{{ $post['Posted by'] }}</td>
+                <td>{{ $post['Created at'] }}</td>
                 <td>
                     <a href="{{ route('posts.show', ['post' => $post['id']]) }}" class="btn btn-info">View</a>
                     <a href="#" class="btn btn-primary">Edit</a>
