f8a16ebe460d5ec718a132ba37c6187752953536
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-04-22 14:00:22 +0200

    auth routes

--- blogDemo/routes/web.php
+++ blogDemo/routes/web.php
@@ -18,19 +18,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/create/', [PostController::class, 'create'])->name('posts.create');
-Route::get('/posts/edit/{post}', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/edit', [PostController::class, 'update'])->name('posts.update');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('/posts/delete/{id}', [PostController::class, 'delete'])->name('posts.delete');
-Route::post('/posts/retrieve/{id}', [PostController::class, 'rollback'])->name('posts.retrieve');
-Route::post('/comments/{id}', [CommentController::class, 'create'])->name('comment.create');
-Route::delete('/comments/delete/{id}', [CommentController::class, 'delete'])->name('comment.delete');
-Route::post('/comments/retrieve/{id}', [CommentController::class, 'rollback'])->name('comment.retrieve');
-Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comment.update');
-
-
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/posts/create/', [PostController::class, 'create'])->name('posts.create');
+    Route::get('/posts/edit/{post}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/edit', [PostController::class, 'update'])->name('posts.update');
+    Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::delete('/posts/delete/{id}', [PostController::class, 'delete'])->name('posts.delete');
+    Route::post('/posts/retrieve/{id}', [PostController::class, 'rollback'])->name('posts.retrieve');
+    Route::post('/comments/{id}', [CommentController::class, 'create'])->name('comment.create');
+    Route::delete('/comments/delete/{id}', [CommentController::class, 'delete'])->name('comment.delete');
+    Route::post('/comments/retrieve/{id}', [CommentController::class, 'rollback'])->name('comment.retrieve');
+    Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comment.update');
+});
+Auth::routes();
