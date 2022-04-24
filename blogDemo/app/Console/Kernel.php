c80e7bfe7ee3fb55efe945cd21ac389ad6b62377
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.ed.eg>
2022-04-24 18:10:08 +0200

    Authinticate api endpoints using Sancutum tokens

--- blogDemo/app/Console/Kernel.php
+++ blogDemo/app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('queue:work')->everyMinute();
+        $schedule->command('sanctum:prune-expired --hours=24')->daily();
     }
 
     /**
--- blogDemo/routes/api.php
+++ blogDemo/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\api\PostController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,12 +18,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+
+        return $request->user();
+    });
+    Route::get('posts', [PostController::class, 'index'])->name('api.posts.index');
+    Route::get('posts/{id}', [PostController::class, 'show'])->name('api.posts.show');
+    Route::post('posts', [PostController::class, 'store'])->name('api.posts.store');
+    Route::delete('posts/{id}', [PostController::class, 'delete'])->name('api.posts.delete');
+    Route::put('posts/{id}', [PostController::class, 'update'])->name('api.posts.update');
 });
-Route::get('posts', [PostController::class, 'index'])->name('api.posts.index');
-Route::get('posts/{id}', [PostController::class, 'show'])->name('api.posts.show');
-Route::post('posts', [PostController::class, 'store'])->name('api.posts.store');
-Route::delete('posts/{id}', [PostController::class, 'delete'])->name('api.posts.delete');
-Route::put('posts/{id}', [PostController::class, 'update'])->name('api.posts.update');
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->email)->plainTextToken;
+});
+
 
